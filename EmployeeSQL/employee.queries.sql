2d72bc127a550f602c25f40b7a8dbf2dd1dd3ca1
mtkrebs87 <144370108+mtkrebs87@users.noreply.github.com>
2023-11-20 22:29:16 -0500

    update queries
    finished employee queries

--- EmployeeSQL/employee.queries.sql
+++ EmployeeSQL/employee.queries.sql
@@ -41,3 +41,43 @@ left join dept_emp
 on dept_emp.dept_no = departments.dept_no
 left join employees
 on dept_emp.emp_no = employees.emp_no
+
+--5.List first name, last name, and sex of each employee whose first name is Hercules and whose last name begins with the letter B
+select employees.first_name,
+	employees.last_name,
+	employees.sex
+from employees
+where first_name = 'Hercules' and left(last_name, 1) = 'B'
+
+--6.List each employee in the Sales department, including their employee number, last name, and first name
+select departments.dept_no,
+	departments.dept_name,
+	dept_emp.emp_no,	
+	employees.last_name,
+	employees.first_name
+from departments
+left join dept_emp 
+on dept_emp.dept_no = departments.dept_no
+left join employees
+on dept_emp.emp_no = employees.emp_no
+where dept_name = 'Sales'
+
+--7.List each employee in the Sales and Development departments, including their employee number, last name, first name, and department name
+select departments.dept_no,
+	departments.dept_name,
+	dept_emp.emp_no,	
+	employees.last_name,
+	employees.first_name
+from departments
+left join dept_emp 
+on dept_emp.dept_no = departments.dept_no
+left join employees
+on dept_emp.emp_no = employees.emp_no
+where dept_name = 'Sales' or dept_name = 'Development'
+
+--8.List the frequency counts, in descending order, of all the employee last names (that is, how many employees share each last name)
+select last_name, count(last_name)as frequency
+from employees
+group by last_name
+order by
+count(last_name)desc
